e82648cd0d638da108ea86c0052606f93c7f4095
stefanocravotta <you@example.com>
2022-04-21 16:44:45 +0200

    livelli di difficoltà

--- script.js
+++ script.js
@@ -13,16 +13,59 @@ const container = document.querySelector('.sc-container');
 const normalDifficultMax = 100;
 const hardDifficultMax = 81;
 const legendaryDifficultMax = 49;
+const userChoice = document.getElementById('choice').value;
 
-init();
+document.querySelector('button').addEventListener('click', function(){
+    
+    if(userChoice === "normal"){
+        initNormal();
+    }else if(userChoice === "hard"){
+        initHard();
+    }else if(userChoice === "legendary"){
+        initLegendary();
+    }
+})
 
-function init(){
+
+
+function initNormal(){
 
     for(let i = 1; i <= normalDifficultMax; i++){
         const sq = createSquare(container);
         console.log(sq);
         const numero = [i];
         console.log([i]);
+        sq.className = 'square sc-100';
+        sq.innerHTML =`<span>${numero}</span>`;
+        sq.addEventListener('click', function(){
+            this.classList.add('clicked');
+        })
+        
+    }
+}
+function initHard(){
+
+    for(let i = 1; i <= hardDifficultMax; i++){
+        const sq = createSquare(container);
+        console.log(sq);
+        const numero = [i];
+        console.log([i]);
+        sq.className = 'square sc-81';
+        sq.innerHTML =`<span>${numero}</span>`;
+        sq.addEventListener('click', function(){
+            this.classList.add('clicked');
+        })
+        
+    }
+}
+function initLegendary(){
+
+    for(let i = 1; i <= legendaryDifficultMax; i++){
+        const sq = createSquare(container);
+        console.log(sq);
+        const numero = [i];
+        console.log([i]);
+        sq.className = 'square sc-49';
         sq.innerHTML =`<span>${numero}</span>`;
         sq.addEventListener('click', function(){
             this.classList.add('clicked');
@@ -35,7 +78,7 @@ function init(){
 
 function createSquare(target){
     const sq = document.createElement('div');
-    sq.className = 'square sc-100'; 
     target.append(sq)
     return sq;
 }
+
